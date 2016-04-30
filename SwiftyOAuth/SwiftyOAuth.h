33b4e914f2d63144a4ce6da2a80b0b63299c49f4
delba <delba@users.noreply.github.com>
2016-04-30 21:04:33 +0200

    Initial Commit

---
+++ SwiftyOAuth/SwiftyOAuth.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyOAuth.h
+//  SwiftyOAuth
+//
+//  Created by Damien on 30/04/2016.
+//  Copyright © 2016 delba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyOAuth.
+FOUNDATION_EXPORT double SwiftyOAuthVersionNumber;
+
+//! Project version string for SwiftyOAuth.
+FOUNDATION_EXPORT const unsigned char SwiftyOAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyOAuth/PublicHeader.h>
+
+
