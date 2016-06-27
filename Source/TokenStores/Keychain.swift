f202f73abeaeb1ea95b3b071cf33b834e37919e8
delba <delba@users.noreply.github.com>
2016-06-27 13:53:31 +0200

    Add MIT license

--- Source/TokenStores/Keychain.swift
+++ Source/TokenStores/Keychain.swift
@@ -1,16 +1,31 @@
 //
-//  Keychain.swift
+// Keychain.swift
 //
-//  Based on Gist: https://gist.github.com/jackreichert/414623731241c95f0e20
+// Copyright (c) 2016 Damien (http://delba.io)
+// Based on Gist: https://gist.github.com/jackreichert/414623731241c95f0e20
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 //
 
-import Foundation
-
-import UIKit
 import Security
 
 class Keychain {
-    
     class func save(key: String, data: NSData) -> Bool {
         let query = [
             kSecClass as String       : kSecClassGenericPassword as String,
@@ -51,7 +66,6 @@ class Keychain {
         
         return status == noErr
     }
-    
     
     class func clear() -> Bool {
         let query = [ kSecClass as String : kSecClassGenericPassword ]
