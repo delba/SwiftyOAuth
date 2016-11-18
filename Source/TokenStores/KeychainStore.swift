f32c21ff99885fa88e7f8577ac119826d08d7df6
delba <delba@users.noreply.github.com>
2016-11-18 18:41:37 +0100

    Add KeychainStore

---
+++ Source/TokenStores/KeychainStore.swift
@@ -0,0 +1,43 @@
+//
+// KeychainStore.swift
+//
+// Copyright (c) 2016 Damien (http://delba.io)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+extension Keychain: TokenStore {
+    public func getTokenForProvider(_ provider: Provider) -> Token? {
+        let key = keyForProvider(provider)
+        
+        guard let dictionary = Keychain.load(key: key) else { return nil }
+        
+        return Token(dictionary: dictionary)
+    }
+    
+    public func setToken(_ token: Token?, forProvider provider: Provider) {
+        let key = keyForProvider(provider)
+        
+        if let token = token {
+            Keychain.save(key: key, dictionary: token.dictionary)
+        } else {
+            Keychain.delete(key: key)
+        }
+    }
+}
